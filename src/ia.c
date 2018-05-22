ee9270845254881ccb1cd387b1ab26ba70649007
TaoGrolleau <tao.grolleau@openmailbox.org>
2018-05-22 11:54:59 +0200

    Changements mineurs

--- src/ia.c
+++ src/ia.c
@@ -3,21 +3,98 @@
 int choix_case(point depart, point arrive) {
     int retour = 0;
     
-    if (arrive.x > depart.x && arrive.y > - arrive.x && arrive.y < arrive.x) {
-        //La case arrive se trouve à droite de la case depart
-        retour = 1;
+    if (arrive.x > depart.x && arrive.x > arrive.y && - arrive.x < arrive.y) {
+        if (carte[(int)depart.x+1][(int)depart.y] != 1) {
+            //La case arrive se trouve à droite de la case depart
+            retour = 1;
+        }
+        else {
+            //On ne peut pas aller à droite
+            retour = 0;
+        }
     }
-    else if (arrive.y < depart.y && arrive.x < arrive.y && arrive.x > - arrive.y) {
-        //La case arrive se trouve en dessous de la case depart
-        retour = 2;
+    else if (arrive.y < depart.y && (arrive.x < - arrive.y || - arrive.x < - arrive.y)) {
+        if (carte[(int)depart.x][(int)depart.y-1] != 1) {
+            //La case arrive se trouve en dessous de la case depart
+            retour = 2;
+        }
+        else {
+            retour = 0;
+        }
     }
-    else if (arrive.x < depart.x && arrive.y > - arrive.x && arrive.y < arrive.x) {
-        //La case arrive se trouve à gauche de la case depart
-        retour = 3;
+    else if (arrive.x < depart.x && (- arrive.x > - arrive.x || - arrive.x < arrive.x)) {
+        if (carte[(int)depart.x-1][(int)depart.y] != 1) {
+            //La case arrive se trouve à gauche de la case depart
+            retour = 3;
+        }
+        else {
+            retour = 0;
+        }
     }
     else if (arrive.y > depart.y && arrive.x < arrive.y && arrive.x > - arrive.y) {
-        //La case arrive se trouve en ahut de la case depart
-        retour = 4;
+        if (carte[(int)depart.x][(int)depart.y+1] != 1) {
+            //La case arrive se trouve en ahut de la case depart
+            retour = 4;
+        }
+        else {
+            retour = 0;
+        }
+    }
+
+    return retour;
+}
+
+int choix_casev2(point depart, point arrive) {
+    int retour = 0;
+
+    if (arrive.x >= depart.x) {
+        //Moitié de droite
+        if (arrive.y >= depart.y) {
+            //Quart haut-droite
+            if (abs(arrive.x) > abs(arrive.y)) {
+                //printf("haut droite droite\n");
+                retour = 1;
+            }
+            else {
+                //printf("haut droite haut\n");
+                retour = 4;
+            }
+        }
+        else {
+            //Quart bas-droite
+            if (abs(arrive.x) > abs(arrive.y)) {
+                //printf("bas droite droite\n");
+                retour = 1;
+            }
+            else {
+                //printf("bas droite bas\n");
+                retour = 2;
+            }
+        }
+    }
+    else {
+        if (arrive.y >= depart.y) {
+            //Quart haut-gauche
+            if (abs(arrive.y) > abs(arrive.x)) {
+                //printf("haut gauche haut\n");
+                retour = 4;
+            }
+            else {
+                //printf("haut gauche gauche\n");
+                retour = 3;
+            }
+        }
+        else {
+            //Quart bas-gauche
+            if (abs(arrive.y) > abs(arrive.x)) {
+                //printf("bas gauche bas\n");
+                retour = 2;
+            }
+            else {
+                //printf("bas gauche gauche\n");
+                retour = 3;
+            }
+        }
     }
 
     return retour;
@@ -70,7 +147,7 @@ void mouvement_ia(point ia[]) {
     nouveau.y = ia[0].y;
     nouveau.z = ia[0].z;
 
-    switch(choix_case(ia[0], cible)){
+    switch(choix_casev2(ia[0], cible)){
     case(1):
         nouveau.x += 1;
         break;
@@ -91,5 +168,23 @@ void mouvement_ia(point ia[]) {
 }
 
 int main () {
-    printf("ok\n");
+    int i;
+    point ia[TAILLE_MAX];
+
+    for (i = 0; i < TAILLE_MAX; i++) {
+        snake[i][0] = 0;
+        snake[i][1] = 0;
+        snake[i][2] = 0;
+
+        ia[i].x = 10;
+        ia[i].y = 20;
+        ia[i].z = 10;
+    }
+
+    mouvement_ia(ia);
+    /*
+    for (i = 0; i < TAILLE_MAX; i++) {
+        printf("%f %f %f\n", ia[i].x, ia[i].y, ia[i].z);
+    }
+    */
 }

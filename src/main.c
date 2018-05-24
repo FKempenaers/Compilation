9cf98ad0dc1da16b58dc752e54d6a250b6e0c41b
jouvetlucas@yahoo.fr <jouvetlucas@yahoo.fr>
2018-05-24 09:45:34 +0200

    modifié :         src/main.c

--- src/main.c
+++ src/main.c
@@ -20,19 +20,11 @@ int main (int argc, char* argv[]){
   SDL_WM_SetCaption("SnakeVSold",NULL);
   ecran = SDL_SetVideoMode(TAILLE_X,TAILLE_Y,32,SDL_HWSURFACE | SDL_GL_DOUBLEBUFFER | SDL_OPENGL);
 
-<<<<<<< HEAD
   liste_map = creer_liste_vide();
   gencarte(15);
   genCarteOK = 1;
   idmap = 0;
 
-=======
-  if(genCarteOK != 0 && genCarteOK != 1) genCarteOK = 0;
-  if(genCarteOK == 0){
-    gencarte(15);
-    genCarteOK = 1;
-  }
->>>>>>> refs/remotes/origin/dev
   srand(getpid());
   
   ia = creer_ia(RAYON);;

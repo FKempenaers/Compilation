436589f1dffbfdb636063c17ee5505701eb35aae
fkempenaers <francis.kempenaers@gmail.com>
2018-05-24 12:49:41 +0200

    menu 	deleted:    bin/snake 	modified:   src/menu.c

--- src/menu.c
+++ src/menu.c
@@ -23,9 +23,13 @@ void menu(){
 void ligne(int x1,int y1,int y2){
   int i,j;
   glColor3ub(255,255,255);
+  glBegin(GL_POINTS);
   for(i = y1;i < y2;i++){
-    glBegin(GL_POINTS);
-    
-    glEnd();
+    glVertex2i(50+i,50+i);
+    glVertex2i(600+i,0+i);
+    glVertex2i(600+i,700+i);
+    glVertex2i(0+i,700+i);
+
   }
+  glEnd();
 }

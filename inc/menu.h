583f3b4724de543f77c4fe04e966cd522b0735a5
jouvetlucas@yahoo.fr <jouvetlucas@yahoo.fr>
2018-05-24 17:50:52 +0200

    modifié :         inc/menu.h 	modifié :         src/menu.c

--- inc/menu.h
+++ inc/menu.h
@@ -3,8 +3,8 @@
 #include <GL/glut.h>
 #include "SDL/SDL.h"
 
-//int Diff;
-//int nbetage;
+int diff;
+int nbetage;
 void menu();
 void bouton(int,int,char *string);
 void boutoncadre(int,int,int,int);
@@ -12,4 +12,6 @@ void Gestionclic(int,int,int,int);
 void jouerM();
 void scoreM();
 int in(int*,int,int);
+void editbox(int x1,int y1);
+void readtextbox(int x1,int y1,char* string);
 
--- src/menu.c
+++ src/menu.c
@@ -1,10 +1,11 @@
 #include "../inc/menu.h"
 
-int jouer[2];
-int score[2];
 
 void menu(){
   int b = 0;
+  
+  int jouer[2];
+  int score[2];
   SDL_Event event;
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_PROJECTION);
@@ -40,13 +41,18 @@ void menu(){
   if(b == 1){
     jouerM();
   }
-  if(b == 8){
+  if(b == 2){
     scoreM();
   }
 
 }
 
 void jouerM(){
+  int b,i;
+  
+  int jouer[2];
+  char nombre[4];
+  SDL_Event event;
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
@@ -56,10 +62,114 @@ void jouerM(){
   glLoadIdentity();
 
   bouton(450,400,"JOUER");
-  jouer[0] = 450;jouer[1] = 400;
+  jouer[0] = 450;jouer[1] = 200;
+  editbox(500,500);
 
   glFlush();
   SDL_GL_SwapBuffers();
+  i = 0;
+  nombre[0] = '\0';
+  nombre[1] = '\0';
+  b = 0;
+  while(b == 0){
+    SDL_WaitEvent(&event);
+    switch(event.type){
+    case SDL_QUIT:
+      exit(0);
+      break;
+    case SDL_MOUSEBUTTONUP:
+      if(in(jouer,event.button.x,event.button.y)){
+	b = 1;
+      }
+      break;
+    case SDL_KEYDOWN:
+      switch(event.key.keysym.sym){
+      case 8:
+	if(i > -1){
+	  i--;
+	  nombre[i] = '\0';
+	}
+	if(i < 0){
+	  i = 0;
+	}
+	break;
+      case 224:
+	nombre[i] = '0';
+	if(i<3)i++;
+	nombre[i] = '\0';	  
+	break;
+      case 38:
+	nombre[i] = '1';
+	if(i<3)i++;
+	nombre[i] = '\0';
+	break;
+      case 233:
+	nombre[i] = '2';
+	if(i<3)i++;
+	nombre[i] = '\0';
+	break;
+      case 34:
+	nombre[i] = '3';
+	if(i<3)i++;
+	nombre[i] = '\0';
+	break;
+      case 39:
+	nombre[i] = '4';
+	if(i<3)i++;
+	nombre[i] = '\0';
+	break;
+      case 40:
+	nombre[i] = '5';
+	if(i<3)i++;
+	nombre[i] = '\0';
+	break;
+      case 45:
+	nombre[i] = '6';
+	if(i<3)i++;
+	nombre[i] = '\0';
+	break;
+      case 232:
+	nombre[i] = '7';
+	if(i<3)i++;
+	nombre[i] = '\0';
+	break;
+      case 95:
+	nombre[i] = '8';
+	if(i<3)i++;
+	nombre[i] = '\0';
+	break;
+      case 231:
+	nombre[i] = '9';
+	if(i<3)i++;
+	nombre[i] = '\0';
+	break;
+      default:
+	break;
+      }
+      glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      gluOrtho2D(0, 1300, 0, 700);
+
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+      bouton(450,400,"JOUER");
+      editbox(500,500);
+      readtextbox(500,500,nombre);
+      SDL_GL_SwapBuffers();
+    default:
+      break;
+    }
+    
+  }
+  diff = b;
+  nbetage = atoi(nombre);
+  if(nbetage > 100){
+    nbetage = 100;
+  }
+  if(nbetage < 1){
+    nbetage = 1;
+  }
 }
 
 void scoreM(){
@@ -72,23 +182,31 @@ void scoreM(){
   glLoadIdentity();
 
   bouton(450,400,"SCORE");
-  jouer[0] = 450;jouer[1] = 400;
+  //jouer[0] = 450;jouer[1] = 400;
 
   glFlush();
   SDL_GL_SwapBuffers();
 
 }
-
+void readtextbox(int x1,int y1,char* string){
+  int i,j;
+  glColor3d(0.2,0.6,0.2);
+  glRasterPos2i(x1,y1);
+  j = strlen(string);
+  for(i = 0; i < j;i++) glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,string[i]);
+}
 void bouton(int x1,int y1,char* string){
   int i,j;
-  boutoncadre(450,x1+200,y1,y1+100);
+  boutoncadre(x1,x1+200,y1,y1+100);
   glColor3d(0.2,0.6,0.2);
   glRasterPos2i(x1+20,y1+50);
   j = strlen(string);
   for(i = 0; i < j;i++) glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,string[i]);
 
 }
-
+void editbox(int x1,int y1){
+  boutoncadre(x1,x1+100,y1,y1+50);
+}
 void boutoncadre(int x1,int x2,int y1,int y2){
   glColor3ub(255,255,255);
   glBegin(GL_QUADS);
